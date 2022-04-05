19fd85885362235b90f226e9b09b6bf57d3c4db9
Gregory Olive <gregoryolive@outlook.com>
2022-04-05 18:44:09 +0900

    Create v2 of api that allows for maps to be called by slug and characters to be called by map_id

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
       resources :characters
       resources :maps
     end
+    namespace :v2 do
+      resources :characters, param: :map_id
+      resources :maps, param: :slug
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
